2cc0a375c6c32fda55e823fb15b2e1183af9ae01
Cameron Baumgartner <cameron.baumgartner@vcfa.edu>
2021-03-08 20:05:56 -0500

    initial commit

---
+++ __tests__/sort.js
@@ -0,0 +1,60 @@
+const {
+  bubbleSort,
+  selectionSort,
+  insertionSort,
+  mergeSort,
+  quickSort,
+  radixSort,
+} = require('../algos/sort');
+
+describe('Sorting', () => {
+  const unsorted = [5, 1, 4, 3, 2];
+
+  describe('Bubble sort', () => {
+    it('should return an array', () => {
+      expect(bubbleSort(unsorted)).toBeInstanceof(Array);
+    });
+
+    xit('', () => {});
+  });
+
+  xdescribe('Selection sort', () => {
+    it('should return an array', () => {
+      expect(selectionSort(unsorted)).toBeInstanceOf(Array);
+    });
+
+    xit('', () => {});
+  });
+
+  xdescribe('Insertion sort', () => {
+    it('should return an array', () => {
+      expect(insertionSort(unsorted)).toBeInstanceOf(Array);
+    });
+
+    xit('', () => {});
+  });
+
+  xdescribe('Merge sort', () => {
+    it('should return an array', () => {
+      expect(mergeSort(unsorted)).toBeInstanceOf(Array);
+    });
+
+    xit('', () => {});
+  });
+
+  xdescribe('Quick sort', () => {
+    it('should return an array', () => {
+      expect(quickSort(unsorted)).toBeInstanceOf(Array);
+    });
+
+    xit('', () => {});
+  });
+
+  xdescribe('Radix sort', () => {
+    it('should return an array', () => {
+      expect(radixSort(unsorted)).toBeInstanceOf(Array);
+    });
+
+    xit('', () => {});
+  });
+});
---
+++ algos/sort.ts
@@ -0,0 +1,34 @@
+export const bubbleSort = (arr: Array<number>): Array<number> => {
+  return [];
+};
+
+export const selectionSort = (arr: Array<number>): Array<number> => {
+  return [];
+};
+
+export const insertionSort = (arr: Array<number>): Array<number> => {
+  return [];
+};
+
+export const mergeSort = (arr: Array<number>): Array<number> => {
+  return [];
+};
+
+export const quickSort = (arr: Array<number>): Array<number> => {
+  return [];
+};
+
+export const radixSort = (arr: Array<number>): Array<number> => {
+  return [];
+};
+
+/*
+module.exports = {
+  bubbleSort,
+  selectionSort,
+  insertionSort,
+  mergeSort,
+  quickSort,
+  radixSort,
+}
+*/
