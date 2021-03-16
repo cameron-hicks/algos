41aed875d7b09e7a2688365798ca5e62fd5d1170
Cameron Baumgartner <cameron.baumgartner@vcfa.edu>
2021-03-16 19:06:22 -0400

    outlined algos to do

---
+++ algos/arrays.js
@@ -0,0 +1,21 @@
+module.exports = {
+  twoSum: (arr, target) => {
+    // Given an array of integers, return the indices of the (only) two numbers that add up to the target.
+  },
+  threeSum: (arr, target) => {
+    // Given an array of integers, find all unique triplets in the array which add up to the target.
+  },
+  stock: (arr) => {
+    // Given an array where the number at each index i is the price of a stock on date i, and assuming you may buy once and sell once, return an array containing: a purchase date, a sell date, and the maximum profit you can make using those dates.
+  },
+  mergeIntervals: (arr) => {
+    // Given a 2D collection of intervals, merge all overlapping intervals.
+  },
+  spiralMatrix: (matrix) => {},
+  setMatrixTo0: (matrix) => {
+    //Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do this in-place.
+  },
+  coinSum: (coins, amount) => {
+    // Given an array of coin denominations, return the smallest number of coins needed to sum up to the target amount.
+  },
+};
--- algos/trees.js
+++ algos/trees.js
@@ -12,4 +12,24 @@ export class BST {
       return tree.left ? tree.left.add(value) : (tree.left = new BST(value));
     }
   }
+
+  // Return true if the difference between the heights of the left and right subtrees is not greater than 1.
+  balancedBST() {
+    // TODO
+  }
+
+  // Return true if, at each node, this tree's value equals the other tree's value.
+  equals(other) {
+    // TODO
+  }
+
+  // Return true if this is a valid binary search tree.
+  valid() {
+    // TODO
+  }
+
+  // Reverse all the nodes such that at each level, the child that is greater than the parent is on the left, while the child that is less than the parent is on the right.
+  reverse() {
+    // TODO
+  }
 }
