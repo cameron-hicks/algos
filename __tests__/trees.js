f7c9f8652d215854f374385323bababa8776a75d
Cameron Baumgartner <cameron.baumgartner@vcfa.edu>
2021-03-24 14:45:20 -0400

    heaps

--- __tests__/trees.js
+++ __tests__/trees.js
@@ -1,33 +1,106 @@
-const { BST } = require('../algos/trees');
+const { BST, BinaryHeap } = require('../algos/trees');
 
 describe('Trees', () => {
+  xdescribe('Binary Search Trees', () => {
 
-  describe('balancedBST', () => { 
-    it('should return true for null and single-node trees', () => {
-      expect(new BST().balancedBST()).toEqual(true);
-      expect(new BST(0).balancedBST()).toEqual(true);
+    describe('balancedBST', () => { 
+      it('should return true for null and single-node trees', () => {
+        expect(new BST().balancedBST()).toEqual(true);
+        expect(new BST(0).balancedBST()).toEqual(true);
+      })
+  
+      it('should return true when heights of subtrees are <= 1', () => {
+        const equal = new BST(10);
+        equal.add(8);
+        equal.add(11);
+        expect(equal.balancedBST()).toEqual(true);
+  
+        const offBy1 = new BST(10);
+        offBy1.add(8);
+        offBy1.add(6);
+        offBy1.add(11);
+        expect(offBy1.balancedBST()).toEqual(true);
+      })
+  
+      it('should return true when heights of subtrees are > 1', () => {
+        const offByMore = new BST(10);
+        offByMore.add(8);
+        offByMore.add(6);
+        offByMore.add(4);
+        offByMore.add(11);
+        expect(offByMore.balancedBST()).toEqual(false);
+      })
     })
-
-    it('should return true when heights of subtrees are <= 1', () => {
-      const equal = new BST(10);
-      equal.add(8);
-      equal.add(11);
-      expect(equal.balancedBST()).toEqual(true);
-
-      const offBy1 = new BST(10);
-      offBy1.add(8);
-      offBy1.add(6);
-      offBy1.add(11);
-      expect(offBy1.balancedBST()).toEqual(true);
+  })
+  
+  xdescribe('Binary Heaps', () => {
+    let maxHeap, minHeap;
+  
+    beforeAll(() => {
+      maxHeap = new BinaryHeap('max');
+      maxHeap.insert(41);
+      maxHeap.insert(39);
+      maxHeap.insert(33);
+      maxHeap.insert(18);
+      maxHeap.insert(27);
+      maxHeap.insert(12);
+      maxHeap.insert(55);
+  
+      minHeap = new BinaryHeap('min');
+      minHeap.insert(41);
+      minHeap.insert(39);
+      minHeap.insert(33);
+      minHeap.insert(18);
+      minHeap.insert(27);
+      minHeap.insert(12);
+      minHeap.insert(55);
     })
-
-    it('should return true when heights of subtrees are > 1', () => {
-      const offByMore = new BST(10);
-      offByMore.add(8);
-      offByMore.add(6);
-      offByMore.add(4);
-      offByMore.add(11);
-      expect(offByMore.balancedBST()).toEqual(false);
+  
+    describe('insert', () => { 
+      it('should add values to the heap\'s values array', () => {
+        expect(maxHeap.values.length).toEqual(7);
+        expect(minHeap.values.length).toEqual(7);
+      })
+  
+      it('should bubble up added values so that they appear in the correct order in the heap array', () => {
+        expect(maxHeap.values).toEqual([55, 39, 41, 18, 27, 12, 33]);
+        expect(minHeap.values).toEqual([12, 27, 18, 41, 33, 39, 55]);
+      })
+    })
+  
+    describe('extractRoot', () => { 
+      let max, min;
+  
+      beforeAll(() => {
+        max = maxHeap.extractRoot();
+        min = minHeap.extractRoot();
+      })
+  
+      it('should remove the first value from the heap\'s values array', () => {
+        expect(max).toEqual(55);
+        expect(min).toEqual(12);
+        expect(maxHeap.values.length).toEqual(6);
+        expect(minHeap.values.length).toEqual(6);
+      })
+  
+      xit('should rebalance heap', () => {
+        expect(maxHeap.values).toEqual([41, 39, 33, 18, 27, 12]);
+        expect(minHeap.values).toEqual([18, 27, 33, 41, 55, 39]);
+      })
+    })
+  })
+  
+  xdescribe('Priority Queues', () => {
+    describe('', () => {
+      it('', () => {
+  
+      })
+    })
+  
+    xdescribe('', () => {
+      it('', () => {
+        
+      })
     })
   })
 })
--- algos/trees.js
+++ algos/trees.js
@@ -1,4 +1,4 @@
-export class BST {
+class BST {
   constructor(value) {
     this.val = value;
   }
@@ -33,3 +33,142 @@ export class BST {
     // TODO
   }
 }
+
+// TODO: TEST & REFACTOR
+class BinaryHeap {
+  constructor(type) {
+    this.type = type; // 'min' or 'max'
+    this.values = [];
+  }
+
+  swap(i, j) {
+    [this.values[i], this.values[j]] = [this.values[j], this.values[i]];
+  }
+
+  insert(value) {
+    this.values.push(value);
+    let childInd = this.values.length - 1;
+    let parentInd = this.indexOfParent(childInd);
+
+    // add a new value then bubble it up until its position makes a valid binary heap
+    if (this.type === 'min') {
+      // the childInd > 0 check is a way to stop iterating if the new val bubbles up to the root (index 0)
+      while (value < this.values[parentInd] && childInd > 0) {
+        this.swap(childInd, parentInd);
+        childInd = parentInd;
+        parentInd = this.indexOfParent(childInd);
+      }
+    } else {
+      while (value > this.values[parentInd] && childInd > 0) {
+        this.swap(childInd, parentInd);
+        childInd = parentInd;
+        parentInd = this.indexOfParent(childInd);
+      }
+    }
+  }
+
+  // Remove the min or max elem, the root of the tree. Take the deepest, rightmost leaf (the last position in the values array) and put it at the root. Then, similar to bubbling up, let the root sink down to its correct position by comparing it iteratively to its children.
+  extractRoot(value) {
+    const sinkDownMin = () => {
+      let parentInd = 0;
+      let leftChildInd = this.indexOfLeftChild(0);
+      let rightChildInd = leftChildInd + 1;
+      let swapChild;
+
+      // the childInd < this.values.length check is a way to stop iterating if the new val sinks down to the bottom
+      while (
+        (value > this.values[leftChildInd] ||
+          value > this.values[rightChildInd]) &&
+        leftChildInd < this.values.length &&
+        rightChildInd < this.values.length
+      ) {
+        // identify smallest of the two children
+        swapChild =
+          this.values[leftChildInd] < this.values[rightChildInd]
+            ? leftChildInd
+            : rightChildInd;
+        // swap the bubbling-down element with its smallest child
+        this.swap(swapChild, parentInd);
+
+        parentInd = swapChild;
+        leftChildInd = this.indexOfLeftChild(parentInd);
+        rightChildInd = leftChildInd + 1;
+      }
+    };
+
+    const sinkDownMax = () => {
+      let parentInd = 0;
+      let leftChildInd = this.indexOfLeftChild(0);
+      let rightChildInd = leftChildInd + 1;
+      let swapChild;
+
+      while (
+        (value < this.values[leftChildInd] ||
+          value < this.values[rightChildInd]) &&
+        leftChildInd < this.values.length &&
+        rightChildInd < this.values.length
+      ) {
+        swapChild =
+          this.values[leftChildInd] > this.values[rightChildInd]
+            ? leftChildInd
+            : rightChildInd;
+
+        this.swap(swapChild, parentInd);
+        parentInd = swapChild;
+        leftChildInd = this.indexOfLeftChild(parentInd);
+        rightChildInd = leftChildInd + 1;
+      }
+    };
+
+    // TODO: edge case
+    // replace root with last leaf
+    const root = this.values[0];
+    this.values[0] = this.values.pop();
+    if (this.values.length <= 1) return root;
+
+    // sink the new root down until its position leaves the binary heap in a valid state
+    if (this.type === 'min') {
+      sinkDownMin();
+    } else {
+      sinkDownMax();
+    }
+
+    return root;
+  }
+
+  // Given the index of a parent node, its children's position in the heap's values array can be easily predicted using the formula 2i + 1. (The right child would be 2i + 2.)
+  indexOfLeftChild(parentInd) {
+    return 2 * parentInd + 1;
+  }
+
+  // Given the index of a child node, its parent's position in the heap's values array can be easily predicted using the formula (i - 1)/2. Math.floor ensures that both left and right children (which will be at even and odd indices, respectively) produce the same parent index.
+  indexOfParent(childInd) {
+    return Math.floor((childInd - 1) / 2);
+  }
+}
+
+module.exports = { BST, BinaryHeap };
+
+const maxHeap = new BinaryHeap('max');
+maxHeap.insert(41);
+maxHeap.insert(39);
+maxHeap.insert(33);
+maxHeap.insert(18);
+maxHeap.insert(27);
+maxHeap.insert(12);
+maxHeap.insert(55);
+
+const minHeap = new BinaryHeap('min');
+minHeap.insert(41);
+minHeap.insert(39);
+minHeap.insert(33);
+minHeap.insert(18);
+minHeap.insert(27);
+minHeap.insert(12);
+minHeap.insert(55);
+maxHeap.extractRoot();
+minHeap.extractRoot();
+
+// TODO: correct result after extracting root?
+// console.log('maxHeap', maxHeap.values); // [ 33, 39, 41, 18, 27, 12 ]
+// console.log('minHeap', minHeap.values); // [ 55, 27, 18, 41, 33, 39 ]
